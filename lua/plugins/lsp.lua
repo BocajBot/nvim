7d17b385a6e74bd13d740eabab155bf89e7b8aac
Bocajgnuoy <bocajgnuoy@gmail.com>
2024-01-11 14:22:27 -0600

    Fixed lsp suggestions with lsp-zero

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,50 +8,22 @@ return {
         "hrsh7th/cmp-buffer",
         "hrsh7th/nvim-cmp",
         "hrsh7th/cmp-nvim-lua",
-        "ray-x/cmp-treesitter",
-        "FelipeLema/cmp-async-path",
+        "hrsh7th/cmp-path",
         "j-hui/fidget.nvim",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "onsails/lspkind-nvim",
+        "VonHeikemen/lsp-zero.nvim",
     },
     config = function()
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
+        local lsp_zero = require("lsp-zero")
+        lsp_zero.on_attach(function(_, bufnr)
+            lsp_zero.default_keymaps({
+                bufnr = bufnr
+            })
+        end)
 
-        local kind_icons = {
-            Text = "",
-            Method = "󰆧",
-            Function = "󰊕",
-            Constructor = "",
-            Field = "󰇽",
-            Variable = "󰂡",
-            Class = "󰠱",
-            Interface = "",
-            Module = "",
-            Property = "󰜢",
-            Unit = "",
-            Value = "󰎠",
-            Enum = "",
-            Keyword = "󰌋",
-            Snippet = "",
-            Color = "󰏘",
-            File = "󰈙",
-            Reference = "",
-            Folder = "󰉋",
-            EnumMember = "",
-            Constant = "󰏿",
-            Struct = "",
-            Event = "",
-            Operator = "󰆕",
-            TypeParameter = "󰅲",
-        }
+        local cmp = require("cmp")
 
         cmp.setup({
             snippet = {
@@ -59,6 +31,14 @@ return {
                     require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
                 end,
             },
+            sources = cmp.config.sources({
+                { name = "path" },
+                { name = "nvim_lsp" },
+                { name = "nvim_lua" },
+                { name = "luasnip", keyword_length = 2 },
+                { name = "buffer",  keyword_length = 3 },
+            }),
+            formatting = lsp_zero.cmp_format(),
             mapping = cmp.mapping.preset.insert({
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -66,35 +46,6 @@ return {
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp",   priority = 1, keyword_length = 3 },
-                { name = "nvim_lua",   priority = 2, keyword_length = 3 },
-                { name = "luasnip",    priority = 3, keyword_length = 3 },
-                { name = "buffer",     priority = 4, keyword_length = 3 },
-                { name = "treesitter", priority = 5, keyword_length = 3 },
-                { name = "async-path", priority = 6, keyword_length = 3 },
-            }),
-            formatting = {
-                format = function(entry, vim_item)
-                    local lspkind_ok, lspkind = pcall(require, "lspkind")
-                    if not lspkind_ok then
-                        -- From kind_icons array
-                        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-                        -- Source
-                        vim_item.menu = ({
-                            buffer = "[Buffer]",
-                            nvim_lsp = "[LSP]",
-                            luasnip = "[LuaSnip]",
-                            nvim_lua = "[Lua]",
-                            latex_symbols = "[LaTeX]",
-                        })[entry.source.name]
-                        return vim_item
-                    else
-                        -- From lspkind
-                        return lspkind.cmp_format()(entry, vim_item)
-                    end
-                end
-            },
         })
 
         require("fidget").setup()
@@ -106,34 +57,10 @@ return {
                 "tsserver",
             },
             handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-                ["rust_analyzer"] = function()
-                    require 'lspconfig'.rust_analyzer.setup {
-                        cmd = vim.lsp.rpc.connect("127.0.0.1", 27631),
-                        init_options = {
-                            lspMux = {
-                                version = "1",
-                                method = "connect",
-                                server = "rust-analyzer",
-                            },
-                        },
-                    }
-                end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig["lua_ls"].setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                            },
-                        },
-                    })
+                lsp_zero.default_setup,
+                lua_ls = function()
+                    local lua_opts = lsp_zero.nvim_lua_ls()
+                    require('lspconfig').lua_ls.setup(lua_opts)
                 end,
             },
         })
