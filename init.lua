af9892f4e80761f6897f0b71fc0ade92346bd5c5
Bocajgnuoy <bocajgnuoy@gmail.com>
2024-01-11 11:42:02 -0600

    Removed rust_tools due to lack of use

--- init.lua
+++ init.lua
@@ -4,33 +4,15 @@ vim.g.loaded_perl_provider = 0
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
-vim.opt.rtp:prepend(lazypath)
 
+vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
--- {
--- 	"nvim-treesitter/nvim-treesitter-context",
--- 	"nvim-treesitter/playground",
---
--- 	"theprimeagen/refactoring.nvim",
--- 	"mbbill/undotree",
--- 	"tpope/vim-fugitive",
--- 	"stevearc/dressing.nvim",
--- 	"simrat39/rust-tools.nvim",
--- 	"mfussenegger/nvim-dap",
--- 	"gelguy/wilder.nvim",
--- 	"folke/zen-mode.nvim",
--- 	"github/copilot.vim",
--- 	"laytan/cloak.nvim",
--- 	"nvimtools/none-ls.nvim",
--- 	"lukas-reineke/indent-blankline.nvim",
--- 	"lewis6991/impatient.nvim",
--- }
