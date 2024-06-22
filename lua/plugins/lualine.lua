1f62407e54ba8e3ab167c842cc856c3f38c55199
Bocaj <Bocajgnuoy@gmail.com>
2024-06-22 09:51:56 -0500

    Updates Deps

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -107,8 +107,10 @@ return {
         ins('lualine_c', {
             function()
                 local msg = 'No Active Lsp'
+                -- Fix deprecated functions
                 local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
                 local clients = vim.lsp.get_active_clients()
+                --
                 if next(clients) == nil then
                     return msg
                 end
