8367e8f14e9cd5805e75ef71492ca9619b2fa898
Jacob <bocajgnuoy@gmail.com>
2024-06-25 06:54:11 -0500

    Updated deps and added Precognition.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -109,7 +109,7 @@ return {
                 local msg = 'No Active Lsp'
                 -- Fix deprecated functions
                 local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                local clients = vim.lsp.get_active_clients()
+                local clients = vim.lsp.get_clients()
                 --
                 if next(clients) == nil then
                     return msg
