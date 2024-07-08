4741a08b90277122438a710b52831319069277c1
Bocaj Gnuoy <bocaj@Bocajs-MacBook-Air.local>
2024-07-08 09:47:10 -0500

    Updated packages and set osc52 as default clipboard

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -10,26 +10,11 @@ return {
             return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
         end
 
-        -- Use xsel or pbcopy if available otherwise use osc52
-        if vim.fn.executable('xsel') == 1 then
-            vim.g.clipboard = {
-                name = 'xsel',
-                copy = { ['+'] = { 'xsel', '-ib' }, ['*'] = { 'xsel', '-ib' } },
-                paste = { ['+'] = { 'xsel', '-ob' }, ['*'] = { 'xsel', '-ob' } },
-            }
-        elseif vim.fn.executable('pbcopy') == 1 then
-            vim.g.clipboard = {
-                name = 'pbcopy',
-                copy = { ['+'] = { 'pbcopy' }, ['*'] = { 'pbcopy' } },
-                paste = { ['+'] = { 'pbpaste' }, ['*'] = { 'pbpaste' } },
-            }
-        else
-            vim.g.clipboard = {
-                name = 'osc52',
-                copy = { ['+'] = copy, ['*'] = copy },
-                paste = { ['+'] = paste, ['*'] = paste },
-            }
-        end
+        vim.g.clipboard = {
+            name = 'osc52',
+            copy = { ['+'] = copy, ['*'] = copy },
+            paste = { ['+'] = paste, ['*'] = paste },
+        }
 
         -- Now the '+' register will copy to system clipboard using OSC52
         -- vim.keymap.set('n', '<leader>c', '"+y')
