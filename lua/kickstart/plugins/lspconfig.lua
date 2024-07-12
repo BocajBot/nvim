60201ee3cd4c29938c1a8bda1c9a79d53aa99d37
Bocaj Gnuoy <jyoung@agileis.com>
2024-07-12 07:51:03 -0500

    Added default LSPs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -198,6 +198,15 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'rust_analyzer',
+        'tsserver',
+        'pylsp',
+        'bashls',
+        'html',
+        'intelephense',
+        'clangd',
+        'jsonls',
+        'yamlls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
