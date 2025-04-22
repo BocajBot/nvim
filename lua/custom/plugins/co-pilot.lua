0483b216049740b71047a769e102ed775dd99662
Agile Support <support@agileis.com>
2025-04-22 11:50:04 +0000

    Fixed error message for empty plugin table

--- lua/custom/plugins/co-pilot.lua
+++ lua/custom/plugins/co-pilot.lua
@@ -1,7 +1,7 @@
--- return {
---   'github/copilot.vim',
---   config = function()
---     vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-accept-word)')
---     vim.keymap.set('i', '<C-S-Right>', '<Plug>(copilot-accept-line)')
---   end,
--- }
+return {
+  --   'github/copilot.vim',
+  --   config = function()
+  --     vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-accept-word)')
+  --     vim.keymap.set('i', '<C-S-Right>', '<Plug>(copilot-accept-line)')
+  --   end,
+}
